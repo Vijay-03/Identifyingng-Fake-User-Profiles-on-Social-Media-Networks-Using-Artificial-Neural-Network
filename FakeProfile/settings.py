8486295db30d457d392eed5bba0cb3c3f8e6468a
vijay03 <81672149+Vijay-03@users.noreply.github.com>
2021-12-12 21:31:25 +0530

    new update to settings

--- FakeProfile/settings.py
+++ FakeProfile/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nbcywyzw0#(l#0@*v*6atz_n=@q1m6_@!#&u3-*sxe^d@l#!vl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', "fake-profiles-identification-app.azurewebsites.net"]
 # Added
