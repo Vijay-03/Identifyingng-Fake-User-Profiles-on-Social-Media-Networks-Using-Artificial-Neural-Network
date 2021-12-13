fed50db29ae8e48f0d6f4a3e235bfdf599996f3d
vijay03 <81672149+Vijay-03@users.noreply.github.com>
2021-12-13 22:40:09 +0530

    new change

--- FakeProfile/settings.py
+++ FakeProfile/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'nbcywyzw0#(l#0@*v*6atz_n=@q1m6_@!#&u3-*sxe^d@l#!vl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['169.254.129.3', '127.0.0.1', "spot-the-fake.azurewebsites.net"]
+# ALLOWED_HOSTS = ['169.254.129.3', '127.0.0.1', "spot-the-fake-profile.azurewebsites.net"]
+ALLOWED_HOSTS = ['*']
 # Added
 
 
